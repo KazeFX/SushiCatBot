fdcab57cb49cb7d2154cad05fa929cac7edb87b6
KazeFX <1071525+KazeFX@users.noreply.github.com>
2026-07-16 19:33:03 +0200

    Set default channel IDs to 0 in api_tokens.py
    Updated api_tokens.py to initialize channel IDs to 0.

--- init.py
+++ init.py
@@ -6,9 +6,9 @@ import sqlite3
 #Creates the api_tokens.py file to store the bot token and channel id's for functions
 with open("api_tokens.py", "a") as f:
     f.write('BOT_TOKEN = "Insert your bot token here"\n')
-    f.write('BIRTHDAY_MESSAGE_CHANNEL_ID = #Insert your channel id here\n')
+    f.write('BIRTHDAY_MESSAGE_CHANNEL_ID = 0 #Insert your channel id here\n')
     f.write('OPEN_WEATHER_API_KEY = "Insert your api key here"\n')
-    f.write('BOT_TRAP_CHANNEL_ID = #Insert your channel id here\n')
+    f.write('BOT_TRAP_CHANNEL_ID = 0 #Insert your channel id here\n')
 
 
 # Creates and defines bot.db
